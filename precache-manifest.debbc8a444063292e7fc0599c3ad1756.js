7950eeaf02828af3cf1e4e99e913acdfccf97296
programmer-141 <honeysidhardh141@gmail.com>
2020-09-17 00:19:48 +1300

    todo

---
+++ precache-manifest.debbc8a444063292e7fc0599c3ad1756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2228ee39003a15332aa0fccdbe7d034d",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "c165ef001ad4adf04605",
+    "url": "/react-todo-list/static/css/main.77f1f242.chunk.css"
+  },
+  {
+    "revision": "a642875ee1efc3d7e2f1",
+    "url": "/react-todo-list/static/js/2.1faf6076.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.1faf6076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c165ef001ad4adf04605",
+    "url": "/react-todo-list/static/js/main.215e9eb2.chunk.js"
+  },
+  {
+    "revision": "345ac7126bf722b6fb11",
+    "url": "/react-todo-list/static/js/runtime-main.1b4aeefe.js"
+  }
+]);
